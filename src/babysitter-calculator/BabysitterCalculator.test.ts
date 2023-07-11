b2907d28e9d68461626f63f93a3708be6dc3043f
Alex Richards <sox@Isaacs-MacBook-Air.local>
2023-07-11 15:11:49 -0400

    refactored handling after midnight times and basic bedtime validation

--- src/babysitter-calculator/BabysitterCalculator.test.ts
+++ src/babysitter-calculator/BabysitterCalculator.test.ts
@@ -4,31 +4,31 @@ import babysitterCalculator from "./BabysitterCalculator";
 describe("Babysitter Calculator", () => {
     describe("start time validation", () => {
         it("should return time form validation error if start time is not **:**", () => {
-            const returnValue = babysitterCalculator("not proper structure", "17:05");
+            const returnValue = babysitterCalculator("not proper structure", "17:05", "17:04");
 
             expect(returnValue).toBe("Time must be entered as HH:MM.");
         });
 
         it("should return time form validation error if start time is not HH:MM", () => {
-            const returnValue = babysitterCalculator("not:correct", "17:05");
+            const returnValue = babysitterCalculator("not:correct", "17:05", "17:04");
 
             expect(returnValue).toBe("Time must be entered as HH:MM.");
         });
 
         it("should return start time error message if start time is earlier than 5:00pm", () => {
-            const returnValue = babysitterCalculator("16:59", "17:05");
+            const returnValue = babysitterCalculator("16:59", "17:05", "17:04");
 
             expect(returnValue).toBe("Start time must be 5:00pm or later.");
         });
 
         it("should not return start time error message if start time is 5:00pm", () => {
-            const returnValue = babysitterCalculator("17:00", "17:05");
+            const returnValue = babysitterCalculator("17:00", "17:05", "17:04");
 
             expect(returnValue).not.toBe("Start time must be 5:00pm or later.");
         });
 
         it("should not return start time error message if start time is after 5:00pm", () => {
-            const returnValue = babysitterCalculator("17:01", "17:05");
+            const returnValue = babysitterCalculator("17:01", "17:05", "17:04");
 
             expect(returnValue).not.toBe("Start time must be 5:00pm or later.");
         });
@@ -36,63 +36,77 @@ describe("Babysitter Calculator", () => {
 
     describe("end time validation", () => {
         it("should return time form validation error if endtime time is not **:**", () => {
-            const returnValue = babysitterCalculator("17:05", "not proper structure");
+            const returnValue = babysitterCalculator("17:05", "not proper structure", "17:06");
 
             expect(returnValue).toBe("Time must be entered as HH:MM.");
         });
 
         it("should return time form validation error if end time is not HH:MM", () => {
-            const returnValue = babysitterCalculator("17:05", "not:correct");
+            const returnValue = babysitterCalculator("17:05", "not:correct", "17:04");
 
             expect(returnValue).toBe("Time must be entered as HH:MM.");
         });
 
         it("should return end time error message if endtime time hour is earlier than start time", () => {
-            const returnValue = babysitterCalculator("18:00", "17:01");
+            const returnValue = babysitterCalculator("18:00", "17:01", "17:04");
 
             expect(returnValue).toBe("Start time must be earlier than end time.");
         });
 
         it("should return end time error message if endtime time is earlier than start time", () => {
-            const returnValue = babysitterCalculator("17:05", "17:01");
+            const returnValue = babysitterCalculator("17:05", "17:01", "17:04");
 
             expect(returnValue).toBe("Start time must be earlier than end time.");
         });
 
         it("should return end time error message if endtime time is the same time as start time", () => {
-            const returnValue = babysitterCalculator("17:05", "17:05");
+            const returnValue = babysitterCalculator("17:05", "17:05", "17:04");
 
             expect(returnValue).toBe("Start time must be earlier than end time.");
         });
 
         it("should return end time error message if endtime time is before 5pm and later than 4am", () => {
-            const returnValue = babysitterCalculator("17:05", "05:00");
+            const returnValue = babysitterCalculator("17:05", "05:00", "17:04");
 
             expect(returnValue).toBe("End time must be no later than 4:00am");
         });
 
         it("should return end time error message if endtime time is before 5pm and later than 4am hour", () => {
-            const returnValue = babysitterCalculator("17:05", "05:00");
+            const returnValue = babysitterCalculator("17:05", "05:00", "17:04");
 
             expect(returnValue).toBe("End time must be no later than 4:00am");
         });
 
         it("should return end time error message if endtime time is before 5pm and in the 4am hour", () => {
-            const returnValue = babysitterCalculator("17:05", "04:18");
+            const returnValue = babysitterCalculator("17:05", "04:18", "17:04");
 
             expect(returnValue).toBe("End time must be no later than 4:00am");
         });
 
         it("should not return end time error message if endtime time is 4am", () => {
-            const returnValue = babysitterCalculator("17:05", "04:00");
+            const returnValue = babysitterCalculator("17:05", "04:00", "17:04");
 
             expect(returnValue).not.toBe("End time must be no later than 4:00am");
         });
 
         it("should not return end time error message if endtime time is earlier than 4am", () => {
-            const returnValue = babysitterCalculator("17:05", "03:59");
+            const returnValue = babysitterCalculator("17:05", "03:59", "17:04");
 
             expect(returnValue).not.toBe("End time must be no later than 4:00am");
+        });
+    });
+
+    describe("bedTime validation", () => {
+        it("should return time form validation error if bedTime time is not **:**", () => {
+            const returnValue = babysitterCalculator("17:05", "17:06", "not proper structure");
+
+            expect(returnValue).toBe("Time must be entered as HH:MM.");
+        });
+
+        it("should return time form validation error if end time is not HH:MM", () => {
+            const returnValue = babysitterCalculator("17:05",  "17:06", "not:correct");
+
+            expect(returnValue).toBe("Time must be entered as HH:MM.");
         });
     });
 });
--- src/babysitter-calculator/BabysitterCalculator.ts
+++ src/babysitter-calculator/BabysitterCalculator.ts
@@ -1,9 +1,9 @@
-const babysitterCalculator = (startTime: string, endTime: string): string => {
-    if (!(correctTimeString(startTime) && correctTimeString(endTime))) {
+const babysitterCalculator = (startTime: string, endTime: string, bedTime: string): string => {
+    if (!(correctTimeString(startTime) && correctTimeString(endTime) && correctTimeString(bedTime))) {
         return "Time must be entered as HH:MM.";
     }
 
-    const endOrStartTimeError = findEndOrStartTimeErrors(startTime, endTime)
+    const endOrStartTimeError = findEndOrStartTimeErrors(startTime, endTime, bedTime);
     if(endOrStartTimeError){
         return endOrStartTimeError
     }
@@ -15,26 +15,46 @@ const correctTimeString = (time: string): boolean => {
     return time.split(":").length === 2 && Number.isInteger(parseInt(time.split(":")[0])) && Number.isInteger(parseInt(time.split(":")[1]));
 }
 
-const findEndOrStartTimeErrors = (startTime: string, endTime: string): string | null => {
+const findEndOrStartTimeErrors = (startTime: string, endTime: string, bedTime: string): string | null => {
     const startTimeHour = parseInt(startTime.split(":")[0]);
     const endTimeHour = parseInt(endTime.split(":")[0]);
+    const bedTimeHour = parseInt(bedTime.split(":")[0]);
+    
     const startTimeMin = parseInt(startTime.split(":")[1]);
     const endTimeMin = parseInt(endTime.split(":")[1]);
+    const bedTimeMin = parseInt(bedTime.split(":")[1]);
     
-    if (startTimeHour < 17 ) {
+    if (startTimeHour < 17 && startTimeHour > 4) {
         return "Start time must be 5:00pm or later.";
     }
 
-    const endTimeIsEarlierThanStartTime = startTimeHour > endTimeHour || (startTimeHour === endTimeHour && endTimeMin <= startTimeMin);
+
+    if (endTimeHour < 17 && endTimeHour >= 4 && endTime !== "04:00" ) {
+        return "End time must be no later than 4:00am";
+    }
+
+    const adjustedStartTimeHour = getAdjustedTime(startTimeHour);
+    const adjustedEndTimeHour = getAdjustedTime(endTimeHour);
+
+    const endTimeIsEarlierThanStartTime = 
+    (adjustedStartTimeHour > adjustedEndTimeHour)
+    || (adjustedStartTimeHour === adjustedEndTimeHour && endTimeMin <= startTimeMin) 
 
     if (endTimeIsEarlierThanStartTime) {
-        if (endTimeHour < 17 && endTime !== "04:00" && endTimeHour > 3) {
-            return "End time must be no later than 4:00am"
-        }
         return "Start time must be earlier than end time.";
     }
 
+    const adjustedBedTimeHour = getAdjustedTime(bedTimeHour);
+
+    const bedTimeIsEarlierThanStartTime = adjustedStartTimeHour > adjustedBedTimeHour || (adjustedStartTimeHour === adjustedBedTimeHour && bedTimeMin <= startTimeMin);
+    if (bedTimeIsEarlierThanStartTime) {
+        return "Start time must be earlier than bed time.";
+    }
     return null;
+}
+
+const getAdjustedTime = (time: number) => {
+    return time < 17 ? time + 24 : time;
 }
 
 export default babysitterCalculator
