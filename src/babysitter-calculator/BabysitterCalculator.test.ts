94830167af81c0dc765ad70541a9318157f4a141
Alex Richards <sox@Isaacs-MacBook-Air.local>
2023-07-11 10:33:25 -0400

    end time must be later than start time

--- src/babysitter-calculator/BabysitterCalculator.test.ts
+++ src/babysitter-calculator/BabysitterCalculator.test.ts
@@ -2,21 +2,43 @@ import babysitterCalculator from "./BabysitterCalculator";
 
 
 describe("Babysitter Calculator", () => {
-    it("should return start time error message if start time earlier than 5:00pm", () => {
-        const returnValue = babysitterCalculator("16:59");
+    describe("start time", () => {
+        it("should return start time error message if start time is earlier than 5:00pm", () => {
+            const returnValue = babysitterCalculator("16:59", "17:05");
 
-        expect(returnValue).toBe("Start time must be 5:00pm or later.")
+            expect(returnValue).toBe("Start time must be 5:00pm or later.")
+        });
+
+        it("should not return start time error message if start time is 5:00pm", () => {
+            const returnValue = babysitterCalculator("17:00", "17:05");
+
+            expect(returnValue).not.toBe("Start time must be 5:00pm or later.")
+        });
+
+        it("should not return start time error message if start time is after 5:00pm", () => {
+            const returnValue = babysitterCalculator("17:01", "17:05");
+
+            expect(returnValue).not.toBe("Start time must be 5:00pm or later.")
+        });
     });
 
-    it("should not return start time error message if start time is 5:00pm", () => {
-        const returnValue = babysitterCalculator("17:00");
+    describe("end time", () => {
+        it("should return end time error message if endtime time hour is earlier than start time", () => {
+            const returnValue = babysitterCalculator("18:05", "17:01");
 
-        expect(returnValue).not.toBe("Start time must be 5:00pm or later.")
-    })
+            expect(returnValue).toBe("Start time must be earlier than end time.")
+        });
 
-    it("should not return start time error message if start time is after 5:00pm", () => {
-        const returnValue = babysitterCalculator("17:01");
+        it("should return end time error message if endtime time is earlier than start time", () => {
+            const returnValue = babysitterCalculator("17:05", "17:01");
 
-        expect(returnValue).not.toBe("Start time must be 5:00pm or later.")
-    })
+            expect(returnValue).toBe("Start time must be earlier than end time.")
+        });
+
+        it("should return end time error message if endtime time is the same time as start time", () => {
+            const returnValue = babysitterCalculator("17:05", "17:05");
+
+            expect(returnValue).toBe("Start time must be earlier than end time.")
+        });
+    });
 });
--- src/babysitter-calculator/BabysitterCalculator.ts
+++ src/babysitter-calculator/BabysitterCalculator.ts
@@ -1,8 +1,18 @@
-const babysitterCalculator = (startTime: string) => {
-    if(parseInt(startTime.split(":")[0]) >= 17 ) {
-        return "";
+const babysitterCalculator = (startTime: string, endTime: string) => {
+    const startTimeHour = parseInt(startTime.split(":")[0]);
+    const endTimeHour = parseInt(endTime.split(":")[0]);
+    const startTimeMin = parseInt(startTime.split(":")[1]);
+    const endTimeMin = parseInt(endTime.split(":")[1]);
+
+    if (startTimeHour < 17 ) {
+        return "Start time must be 5:00pm or later.";
     }
-    return "Start time must be 5:00pm or later.";
+
+    if (startTimeHour > endTimeHour || (startTimeHour === endTimeHour && endTimeMin <= startTimeMin)) {
+        return "Start time must be earlier than end time.";
+    }
+
+    return "";
 }
 
 export default babysitterCalculator
